cecf2906fc6e3f9c65fb9a0dd7d64eeaf4d5a1cb
anjali-sri-72 <135843451+anjali-sri-72@users.noreply.github.com>
2024-06-19 12:38:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let sqlite3 = require("sqlite3");
 let path = require("path");
 
 let app = express();
+app.use(express.json());
 
 let dbPath = path.join(__dirname, "moviesData.db");
 
@@ -12,7 +13,7 @@ let db = null;
 let initializeDb = async () => {
   try {
     db = await open({
-      fileName: dbPath,
+      filename: dbPath,
       driver: sqlite3.Database,
     });
 
@@ -38,12 +39,12 @@ app.get("/movies/", async (request, response) => {
 //API 2
 
 app.post("/movies/", async (request, response) => {
-  let { director_id, movie_name, lead_actor } = request.body;
+  let { directorId, movieName, leadActor } = request.body;
   let moviesQuery2 = `INSERT INTO 
-  movie_name(director_id, movie_name, lead_actor) 
-  VALUES ('${director_id}',
-   '${movie_name}',
-   '${lead_actor}'); `;
+  movie(director_id, movie_name, lead_actor) 
+  VALUES ('${directorId}',
+   '${movieName}',
+   '${leadActor}'); `;
   let movie2 = await db.run(moviesQuery2);
   response.send("Movie Successfully Added");
 });
@@ -51,22 +52,22 @@ app.post("/movies/", async (request, response) => {
 //API 3
 
 app.get("/movies/:movieId", async (request, response) => {
-  let { movie_id } = request.params;
-  let moviesQuery3 = `SELECT * FROM movie WHERE movie_id = ${movie_id};`;
+  let { movieId } = request.params;
+  let moviesQuery3 = `SELECT * FROM movie WHERE movie_id = ${movieId};`;
   let movie3 = await db.get(moviesQuery3);
   response.send(movie3);
 });
 
 //API 4
 app.put("/movies/:movieId", async (request, response) => {
-  let { movie_id } = request.params;
-  let { director_id, movie_name, lead_actor } = request.body;
+  let { movieId } = request.params;
+  let { directorId, movieName, leadActor } = request.body;
   let moviesQuery4 = `UPDATE 
   movie_name 
-  SET director_id = '${director_id}',
-   movie_name = '${movie_name}',
-   lead_actor ='${lead_actor}'
-   WHERE movie_id = '${movie_id};`;
+  SET director_id = '${directorId}',
+   movie_name = '${movieName}',
+   lead_actor ='${leadActor}'
+   WHERE movie_id = '${movieId};`;
   let movie4 = await db.run(moviesQuery4);
   response.send("Movie Details Updated");
 });
@@ -74,8 +75,8 @@ app.put("/movies/:movieId", async (request, response) => {
 //API 5
 
 app.delete("/movies/:movieId", async (request, response) => {
-  let { movie_id } = request.params;
-  let moviesQuery5 = `DELETE * FROM movie WHERE movie_id = ${movie_id};`;
+  let { movieId } = request.params;
+  let moviesQuery5 = `DELETE * FROM movie WHERE movie_id = ${movieId};`;
   await db.run(moviesQuery5);
   response.send("Movie Removed");
 });
@@ -91,8 +92,8 @@ app.get("/directors/", async (request, response) => {
 //API 7
 
 app.get("/directors/:directorId/movies/", async (request, response) => {
-  let { director_id } = request.params;
-  let moviesQuery7 = `SELECT movie_name FROM movie WHERE director_id = '${director_id};`;
+  let { directorId } = request.params;
+  let moviesQuery7 = `SELECT movie_name FROM movie WHERE director_id = '${directorId};`;
   let director2 = await db.all(moviesQuery7);
   response.send(director2);
 });
